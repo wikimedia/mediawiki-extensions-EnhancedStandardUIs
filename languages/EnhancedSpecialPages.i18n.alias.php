a0a8e1a2026bdc3335a8c1587ef55f8bbf1c48e2
Hua Jing <hua.jing@hallowelt.com>
2024-10-15 11:16:03 +0200

    Hide several special pages from Special:SpecialPages
    Edit overwrites
    
    ERM38624
    
    ERM38821
    
    [4.5.x]
    
    WCAG
    
    Change-Id: I41370ba48aad16969d543ad2219d5c721393af1d
    (cherry picked from commit ccc31e9271808432688d86e60fbe36969a3dc9f1)

---
+++ languages/EnhancedSpecialPages.i18n.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for EnhancedSpecialPages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/**
+ * English
+ */
+$specialPageAliases['en'] = [
+	'EnhancedSpecialPages' => [ 'Special Pages', 'Special pages', 'Specialpages', 'SpecialPages' ],
+];
+
+/**
+ * Deutsch
+ */
+$specialPageAliases['de'] = [
+	'EnhancedSpecialPages' => [ 'Spezialseiten' ],
+];
