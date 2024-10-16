0fd39b865cbcaec73d1323e667fd3185eadef17d
Mschlindwein <schlindwein@hallowelt.com>
2024-10-16 12:09:36 +0200

    Fix global action entry role
    ERM31389
    
    [4.5.x]
    
    Change-Id: I4e1182051caab247cc55cdc59ee7690264a533b5

--- src/FilelistGlobalAction.php
+++ src/FilelistGlobalAction.php
@@ -9,7 +9,9 @@ use SpecialPage;
 class FilelistGlobalAction extends RestrictedTextLink {
 
 	public function __construct() {
-		parent::__construct( [] );
+		parent::__construct( [
+			'role' => 'menuitem'
+		] );
 	}
 
 	/**
