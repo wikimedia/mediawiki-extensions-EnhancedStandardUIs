f42e1b627219f37640c4fb3ac112ad9d0b9cf420
hslater <hamish.slater@hallowelt.com>
2025-02-10 13:35:54 +0100

    Update ExtensionRegistry namespace
    ERM38444
    
    [REL1_43, master]

--- src/Action/EnhancedHistoryAction.php
+++ src/Action/EnhancedHistoryAction.php
@@ -3,7 +3,6 @@
 namespace MediaWiki\Extension\EnhancedStandardUIs\Action;
 
 use ChangeTags;
-use ExtensionRegistry;
 use HistoryAction;
 use InvalidArgumentException;
 use LogEventsList;
@@ -13,6 +12,7 @@ use MediaWiki\Json\FormatJson;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Message\Message;
+use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\Revision\RevisionRecord;
 use RawMessage;
 use Wikimedia\Rdbms\IResultWrapper;
