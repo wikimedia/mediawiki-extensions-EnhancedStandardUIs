c5a4241f97256fcd092ce481354099334036890f
hslater <hamish.slater@hallowelt.com>
2025-05-28 14:32:05 +1200

    Replace deprecated OutputPage::setPreventClickjacking
    ERM38444
    
    [REL1_43, REL1_43-2.0.x, master]
    
    Change-Id: I844e7f5eb06c614b2f1030cd32fc7db085fc0a52
    (cherry picked from commit ba36c1f33c035a1d75da9ad203d647e849c7dc37)

--- src/Special/EnhancedSpecialPages.php
+++ src/Special/EnhancedSpecialPages.php
@@ -122,7 +122,7 @@ class EnhancedSpecialPages extends UnlistedSpecialPage {
 		$out = $this->getOutput();
 		$this->setHeaders();
 		$this->outputHeader();
-		$out->setPreventClickjacking( false );
+		$out->getMetadata()->setPreventClickjacking( false );
 		$out->addModuleStyles( 'mediawiki.special' );
 
 		$groups = $this->getPageGroups();
